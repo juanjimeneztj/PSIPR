428500339fb636ba88a649ca7b2c14731e2cbf8e
Juan Jimenez <juanjimeneztj@gmail.com>
2021-05-18 16:03:06 -0700

    mod

--- psipr/psipr.php
+++ psipr/psipr.php
@@ -1,5 +1,6 @@
 <?php
 
 spl_autoload_register(function ($nombre_clase) {
-    include_once $nombre_clase . '.php';
+    // include_once $nombre_clase . '.php';
+    require __DIR__.'/getURL/'.$nombre_clase.'.php';
 });
