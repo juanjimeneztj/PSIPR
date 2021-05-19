35f7ffb1356124b78ab55430b0a9d89a6e07452f
Juan Jimenez <juanjimeneztj@gmail.com>
2021-05-19 15:30:12 -0700

    testing new params for getURL class

--- class/getParams/getURL.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace App2;
-
-class getURL{
-    public function init(){
-        return 'init desde archivo get URL desde getParams debo ver mis cursos';
-    }
-}
--- index.php
+++ index.php
@@ -1,14 +1,2 @@
 <?php
-require_once 'vendor/autoload.php';
-
-use App\getURL as option1;
-use App2\getURL as option2;
-
-
-$test = new option1();
-echo $test->init();
-
-echo '<br>';
-
-$test2 = new option2();
-echo $test2->init();
+// Silence is golder
